fcabf049392af189cee42fa9aeabaa969e1d5358
EstelaR <esty23@gmail.com>
2015-07-29 12:33:40 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -46,6 +46,13 @@ exports.answer = function(req, res) {
   res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado, errors: [] });
 };
 
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
 // GET /quizes/new
 exports.new = function(req, res) {
 	var quiz = models.Quiz.build( 	// crea el objeto quiz
@@ -73,6 +80,26 @@ exports.create = function(req, res) {
         .save({fields: ["pregunta", "respuesta"]})
         .then( function(){ res.redirect('/quizes')}) 
       }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  );
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
     }
   );
 };
